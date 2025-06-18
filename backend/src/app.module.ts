23506b49a86ce6340d292094bb43bf199d0a59a3
Tiago C. Lima <casmei@protonmail.com>
2025-06-18 12:05:25 -0300

    🔒️ add throttler protection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database';
 import { ProductModule } from './modules/product/product.module';
 import { OrderModule } from './modules/order/order.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { throttlerConfig } from './config/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseConfig), ProductModule, OrderModule],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+    ThrottlerModule.forRoot(throttlerConfig),
+    ProductModule,
+    OrderModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
